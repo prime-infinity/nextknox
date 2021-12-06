b96feb6462d29c5696b9ed2e0090a930f8c1eed0
prime-infinity <soe.a.prime@gmail.com>
2021-12-06 03:14:33 +0100

    added mainimage,in home

---
+++ src/components/MainBody.vue
@@ -0,0 +1,52 @@
+<template>
+
+    <div>
+        <div class="row">
+            <div class="col-12 col-md-7 text-center text-md-left my-auto pt-5 pt-md-0">
+
+                <span class="h1 font-weight-bold">This is an Header An One</span>
+
+                <p class="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus expedita neque odit tempora molestiae odio recusandae consequatur, quisquam iusto dolores assumenda</p>
+
+            </div>
+
+            <div class="col-12 col-md-5">
+                <img class="img-fluid" src="/images/mainImage.png" alt="introimage">
+            </div>
+
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'MainBody',
+  data(){
+      return{
+          
+      }  
+  },   
+    components: {
+    },
+    computed:{
+
+    },  
+    methods: {
+       
+        
+    }, 
+    mounted(){
+        
+    }
+
+}
+</script>
+
+<style>
+  
+</style>
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-   
+    <MainBody />
   </div>
 </template>
 
 <script>
+import MainBody from '../components/MainBody.vue'
 
 export default {
   name: 'Home',
   components: {
-    
+    MainBody,
   }
 }
 </script>
